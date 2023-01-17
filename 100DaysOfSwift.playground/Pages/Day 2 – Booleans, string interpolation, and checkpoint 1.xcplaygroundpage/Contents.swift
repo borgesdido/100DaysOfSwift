4f680e6525f0540995a5776cf9f0f5aa6f51cf2e
Diego Araújo Borges da Silva <didoarorges@gmail.com>
2023-01-17 11:35:56 -0300

    day 2 and checkpoint 1 done

--- 100DaysOfSwift.playground/Pages/Day 2 – Booleans, string interpolation, and checkpoint 1.xcplaygroundpage/Contents.swift
+++ 100DaysOfSwift.playground/Pages/Day 2 – Booleans, string interpolation, and checkpoint 1.xcplaygroundpage/Contents.swift
@@ -1,7 +1,55 @@
-//: [Previous](@previous)
 
 import Foundation
+//MARK: - Booleans
 
-var str = "Hello, playground"
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
 
-//: [Next](@next)
+let number = 120
+print(number.isMultiple(of: 3))
+
+let goodDogs = true
+
+var gameOver = false
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+// MARK: -  How to join strings together
+
+
+let firstPart = "Hello"
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let name = "Diego"
+let age = 31
+let message = "Hello, my name is \(name) and I`m \(age) years old."
+print(message)
+
+let myNumber = 11
+let missionMessage = "Apolo \(myNumber) landed on the moon!"
+
+print ("5 x 5 is \(5 * 5 )")
+
+//MARK: - checkpoint 1
+
+let celsius = 23.0
+let convertFahreinheit = celsius * 9 / 5 + 32
+
+print ("\(celsius)°C is converted to \(convertFahreinheit)°F")
+
