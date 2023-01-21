852526a96822356ef3707dcfc99815a4a5a9cb90
Diego Araújo Borges da Silva <didoarorges@gmail.com>
2023-01-21 15:52:28 -0300

    Checkpoint 5 Done

--- 100DaysOfSwift.playground/Pages/Day 9 – Checkpoint 5.xcplaygroundpage/Contents.swift
+++ 100DaysOfSwift.playground/Pages/Day 9 – Checkpoint 5.xcplaygroundpage/Contents.swift
@@ -1,2 +1,18 @@
 import Foundation
+//This challenge tests your ability to create trailing closures, chained together to solve a problem.
 
+//Given a particular array of numbers, the challenge is this: filter out all numbers that are even, sort the array in ascending order, map them to strings in the format “7 is a lucky number”, then print the resulting array one item per line.
+
+let luckyNumbers  = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+func isOdd(number: Int)-> Bool {
+    number.isMultiple(of: 2) == false
+}
+func luckyString(_ number: Int)-> String {
+    "\(number) is a  lucky number"
+}
+func printValue(_ string: String) {
+    print(string)
+}
+
+luckyNumbers.filter (isOdd).sorted().map(luckyString).forEach (printValue)
