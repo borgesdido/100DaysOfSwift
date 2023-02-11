a381327fc221c135ae2fe5b4da6adfc61f9782e0
Diego Araújo Borges da Silva <didoarorges@gmail.com>
2023-02-11 15:39:28 -0300

    project 4 added

---
+++ RockPaperScissorrs/RockPaperScissorrs/RockPaperScissorrsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorrsApp.swift
+//  RockPaperScissorrs
+//
+//  Created by Juliana Kaminaga Roppa on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorrsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
