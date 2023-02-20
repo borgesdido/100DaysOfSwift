3793f30ef23b60cdba69187b051d8ce39210f8b2
Diego Araújo Borges da Silva <didoarorges@gmail.com>
2023-02-20 14:37:48 -0300

    BetterRest project finished

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,43 +5,82 @@
 //  Created by Diego Araújo Borges on 11/02/23.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUP = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    
-    
+
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Please enter a time", selection: $wakeUP, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in 4...12, step: 0.25)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in 1...20)
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Que horas você quer acordar?")
+                        .font(.headline)
+
+                    DatePicker("Por favor selecione uma data", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Tempo desejado de sono")
+                        .font(.headline)
+
+                    Stepper("\(sleepAmount.formatted()) horas", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Quantidade de café ingerida")
+                        .font(.headline)
+
+                    Stepper(coffeeAmount == 1 ? "1 copo" : "\(coffeeAmount) copos", value: $coffeeAmount, in: 1...20)
+                }
             }
-            
             .navigationTitle("BetterRest")
             .toolbar {
-                Button("Calculate", action: calculateBedTime)
+                Button("Calcular", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
             }
         }
-        
     }
-    
-    func calculateBedTime() {
-        
+
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Sua hora ideial de dormir é:"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+
+        showingAlert = true
     }
 }
 
@@ -50,5 +89,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
 
 
